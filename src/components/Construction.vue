ce35a66f7634c99ae5311fc860f8314cf6855f95
Andreas Stensig <stensig04@gmail.com>
2020-02-24 18:45:40 +0100

    Started construction page

---
+++ src/components/Construction.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <v-icon>home</v-icon>
+      <h2>The site is under construction</h2>
+      <h3>Please check back later</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Construction'
+}
+</script>
+
+<style scoped>
+
+</style>
