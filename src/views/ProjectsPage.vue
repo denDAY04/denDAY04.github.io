352403d376df950bfe5bd1dbb1527aeb0905c932
Andreas Stensig <stensig04@gmail.com>
2020-04-12 14:29:40 +0200

    Projects page styling and minor data changes

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -12,6 +12,7 @@
           v-model="skillsQuery"
           :items="skillsList"
           label="Filter projects by skills"
+          class="mb-n10"
           multiple
           clearable
           chips
@@ -23,33 +24,38 @@
         <v-card
           v-for="project in displayProjects"
           :key="project.title"
-          class="skill-card"
+          class="my-10"
+          raised
         >
-          <v-card-title class="blue-grey darken-4">{{ project.title }}</v-card-title>
-          <v-card-subtitle class="project-affiliate">
+          <v-card-title class="blue-grey darken-4">
+            {{ project.title }}
+          </v-card-title>
+          <v-card-subtitle class="mt-2 overline">
             {{ project.affiliate }} project
           </v-card-subtitle>
           <v-card-text>
             {{ project.description}}
           </v-card-text>
-          <v-card-actions v-if="project.links !== undefined" class="project-links">
-            <v-item-group>
-              <v-item>
-                <v-icon class="project-link">mdi-link</v-icon>
-              </v-item>
-              <v-item class="project-link" v-for="link in project.links" :key="link.label">
-                <a :href="link.href" target="_blank">{{ link.label }}</a>
-              </v-item>
-            </v-item-group>
+          <v-card-actions v-if="projectHasLinks(project)" class="mt-n3">
+            <v-btn
+              v-for="link in project.links" :key="link.label"
+              color="teal lighten-2"
+              @click="openProjectLink(link)"
+              text
+            >
+              {{ link.label }}
+            </v-btn>
           </v-card-actions>
           <v-divider/>
-          <v-card-actions>
-            <div class="text-uppercase skills">Role</div>
-            <span class="font-italic">{{ printRoles(project.role) }}</span>
-          </v-card-actions>
+          <v-card-text>
+            <v-icon class="mr-2" title="Role">mdi-shield-account</v-icon>
+            <span class="font-weight-thin">{{ printRoles(project) }}</span>
+
+            <v-icon class="mx-2" title="Year">mdi-clock-outline</v-icon>
+            <span class="font-weight-thin">{{ printTime(project) }}</span>
+          </v-card-text>
           <v-divider/>
-          <v-card-actions>
-            <div class="text-uppercase skills">Skills</div>
+          <v-card-text>
             <v-chip-group column>
               <v-chip
                 v-for="skill in project.skills"
@@ -59,7 +65,7 @@
                 {{ skill }}
               </v-chip>
             </v-chip-group>
-          </v-card-actions>
+          </v-card-text>
         </v-card>
       </v-card-text>
     </v-col>
@@ -85,10 +91,14 @@ export default {
     const vm = this;
     axios.get('data/projects.json').then((response) => {
       vm.projects = response.data.sort((a, b) => {
-        if (a.title > b.title) return 1;
-        if (a.title < b.title) return -1;
+        if (a.start > b.start) return 1;
+        if (a.start < b.start) return -1;
+
+        if (a.end > b.start) return 1;
+        if (a.start < b.end) return -1;
+
         return 0;
-      });
+      }).reverse();
 
       vm.projects.forEach((p) => {
         p.skills.sort();
@@ -140,8 +150,8 @@ export default {
         window.eventBus.$emit('message', { type: 'I', msg: 'Already filtering on this skill' });
       }
     },
-    printRoles(roleList) {
-      const it = roleList[Symbol.iterator]();
+    printRoles(project) {
+      const it = project.role[Symbol.iterator]();
       let role = it.next();
       let text = role.value;
       role = it.next();
@@ -152,24 +162,23 @@ export default {
       }
       return text;
     },
+    printTime(project) {
+      let text = project.start;
+      if (project.end !== undefined) {
+        text += ` - ${project.end}`;
+      }
+      return text;
+    },
+    openProjectLink(link) {
+      window.open(link.href, '_blank');
+    },
+    projectHasLinks(project) {
+      return project.links !== undefined && project.links.length > 0;
+    },
   },
 };
 </script>
 
 <style scoped>
-.skill-card {
-  margin-top: 3em;
-}
-.skills {
-  margin-right: 1em;
-}
-.project-link {
-  margin-left: 1em;
-}
-.project-affiliate {
-  margin-top: 1em;
-}
-.project-links {
-  margin-top: -1em;
-}
+
 </style>
