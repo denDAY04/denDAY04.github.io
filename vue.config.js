5464804c1a448e8fe766a31fe9f8f76c77e7f624
Andreas Stensig <stensig04@gmail.com>
2020-05-21 15:48:32 +0200

    Sitemap generator

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+const paths = [
+  '/',
+  '/#/about',
+  '/#/experience',
+  '/#/projects',
+];
+
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin('https://stensig.net', paths, { lastmod: true }),
+    ],
+  },
 };
